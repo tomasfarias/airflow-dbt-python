2e61ca96cb5c7a490f6f5dcab264afee0a25af3b
Tomás Farías Santana <tomas@tomasfarias.dev>
2021-09-22 23:19:27 +0200

    ci: Build package before building Sphinx docs

--- airflow_dbt_python/__init__.py
+++ airflow_dbt_python/__init__.py
@@ -1,1 +1,2 @@
 """Provides an Airflow operator and hooks to run all or most dbtcommands."""
+from .__version__ import __author__, __copyright__, __title__, __version__
--- airflow_dbt_python/__version__.py
+++ airflow_dbt_python/__version__.py
@@ -1,1 +1,4 @@
+__author__ = "Tomás Farías Santana"
+__copyright__ = "Copyright 2021 Tomás Farías Santana"
+__title__ = "airflow-dbt-python"
 __version__ = "0.7.2"
