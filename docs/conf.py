3c34c74e350657b41494df2964b75ea1e19bde6d
Tomás Farías Santana <tomas@tomasfarias.dev>
2021-09-22 03:14:01 +0200

    ci: Update GitHub action for documentation deployment

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,12 @@ release = "0.7.2"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
