1cffdcafd8841b96ee622a49d9ae8f6716c55831
GitHub Action <tomas@tomasfarias.dev>
2021-12-23 18:39:06 +0100

    docs: Minor doc updates to mention support for dbt docs

--- examples/dbt_project_in_s3_dag.py
+++ examples/dbt_project_in_s3_dag.py
@@ -3,7 +3,7 @@ import datetime as dt
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from airflow_dbt_python.dbt.operators import DbtRunOperator
+from airflow_dbt_python.dbt.operators import DbtDocsGenerateOperator, DbtRunOperator
 
 with DAG(
     dag_id="example_basic_dbt_run_with_s3",
@@ -12,6 +12,7 @@ with DAG(
     catchup=False,
     dagrun_timeout=dt.timedelta(minutes=60),
 ) as dag:
+    # Project files will be pulled from "s3://my-bucket/dbt/profiles/key/prefix/"
     dbt_run = DbtRunOperator(
         task_id="dbt_run_hourly",
         project_dir="s3://my-bucket/dbt/project/key/prefix/",
@@ -22,3 +23,13 @@ with DAG(
         profile="my-project",
         full_refresh=False,
     )
+
+    # Documentation files (target/manifest.json, target/index.html, and
+    # target/catalog.json) will be pushed back to S3 after compilation is done.
+    dbt_docs = DbtDocsGenerateOperator(
+        task_id="dbt_run_hourly",
+        project_dir="s3://my-bucket/dbt/project/key/prefix/",
+        profiles_dir="s3://my-bucket/dbt/profiles/key/prefix/",
+    )
+
+    dbt_run >> dbt_docs
