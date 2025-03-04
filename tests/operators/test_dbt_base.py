f2ffd2894d0fe17c218f3ab71d09ef46b4d2bdf1
Alexander Millin <a.v.millin@breezeventures.io>
2025-03-04 15:04:48 +0300

    docs: update docstring

--- tests/operators/test_dbt_base.py
+++ tests/operators/test_dbt_base.py
@@ -7,6 +7,5 @@ from airflow_dbt_python.operators.dbt import DbtBaseOperator
 
 def test_dbt_base_does_not_implement_command():
     """Test DbtBaseOperator doesn't implement a command."""
-    op = DbtBaseOperator(task_id="dbt_task")
-    with pytest.raises(NotImplementedError):
-        op.command
+    with pytest.raises(TypeError):
+        DbtBaseOperator(task_id="dbt_task")
