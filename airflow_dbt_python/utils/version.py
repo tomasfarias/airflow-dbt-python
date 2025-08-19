576b27befda8f63b5fc53b17aaacef80d52a2821
Tomás Farías Santana <tomas@tomasfarias.dev>
2025-08-19 10:51:24 +0200

    fix: Pass empty list as 'active_integrations' in 'parse_manifest'
    This should allow code running dbt 1.10.7 or later not to break.
    
    We still haven't added actual support for whatever this argument is
    doing, this is just ensuring we don't crash by setting the necessary
    argument. Some investigation is required into the new feature/argument
    to figure out what to pass to it.

--- airflow_dbt_python/utils/version.py
+++ airflow_dbt_python/utils/version.py
@@ -16,11 +16,15 @@ DBT_1_8 = VersionSpecifier(
 DBT_1_9 = VersionSpecifier(
     major="1", minor="9", patch="0", matcher=Matchers.GREATER_THAN_OR_EQUAL
 )
+DBT_1_10_7 = VersionSpecifier(
+    major="1", minor="10", patch="7", matcher=Matchers.GREATER_THAN_OR_EQUAL
+)
 DBT_2_0 = VersionSpecifier(
     major="2", minor="0", patch="0", matcher=Matchers.GREATER_THAN_OR_EQUAL
 )
 
 DBT_INSTALLED_GTE_1_9 = installed.compare(DBT_1_9) == 1
+DBT_INSTALLED_GTE_1_10_7 = installed.compare(DBT_1_10_7) == 1
 
 DBT_INSTALLED_1_8 = DBT_1_8 < installed < DBT_1_9
 DBT_INSTALLED_1_9 = DBT_1_9 < installed < DBT_2_0
